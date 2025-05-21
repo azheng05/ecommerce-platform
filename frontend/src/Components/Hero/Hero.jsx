dc601c379ef03e6466cfc653f91543e756773228
Anna Zheng <azheng38@wisc.edu>
2025-05-21 18:26:39 -0500

    Added the navigation bar and set up the routing to different pages. Started working on hero section.

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>New Arrivals</h2>
+            <div className="hero-hand-icon">
+                <p>new</p>
+            </div>
+        </div>
+        <div className="hero-right">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/ShopImages/CatLogo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
+
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>Koshka Crochets</p>
+        </div>
+        <ul className="nav-menu">
+            {/*When you click on a section, it will underline it*/}
+            <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("plushies")}}><Link style={{ textDecoration: 'none' }} to='/plushies'>Plushies</Link>{menu==="plushies"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("accessories")}}><Link style={{ textDecoration: 'none' }} to='/accessories'>Accessories</Link>{menu==="accessories"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("keychains")}}><Link style={{ textDecoration: 'none' }} to='/keychains'>Keychains</Link>{menu==="keychains"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link to="/login"><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">
+                0
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
