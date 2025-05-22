681ce43fe253c30bf381f7643f954f90bebad872
Anna Zheng <azheng38@wisc.edu>
2025-05-22 13:49:51 -0500

    Finished new products section

--- frontend/src/Components/NewProducts/NewProducts.jsx
+++ frontend/src/Components/NewProducts/NewProducts.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './NewProducts.css'
+import new_products from '../Assets/new_collections'
+import Item from '../Item/Item'
 
 const NewProducts = () => {
   return (
-    <div>NewProducts</div>
+    <div className='new-products'>
+        <h1>NEW PRODUCTS</h1>
+        <hr/>
+        <div className="products">
+            {new_products.map((item, i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
+        </div>
+    </div>
   )
 }
 
