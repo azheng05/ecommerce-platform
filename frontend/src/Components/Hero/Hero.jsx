0ef07b5361893c1f46936f81d1d3d30f13e99d43
Anna Zheng <azheng38@wisc.edu>
2025-05-22 10:01:27 -0500

    Added hero section. Edited the colors of the website.

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 export const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-left">
             <h2>New Arrivals</h2>
-            <div className="hero-hand-icon">
-                <p>new</p>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>NEW</p>
+                    <img src={hand_icon} alt=""/>
+                </div>
+                <p>Crochet</p>
+                <p>Creations</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Collection</div>
+                <img arc={arrow_icon} alt=""/>
             </div>
         </div>
         <div className="hero-right">
-
+            <img src={hero_image} alt=""/>
         </div>
     </div>
   )
