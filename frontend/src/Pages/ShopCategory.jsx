eb88330a66db2ebcc7d33eb9d2bec079b125f16f
Anna Zheng <azheng38@wisc.edu>
2025-05-25 16:43:26 -0500

    Added load more button

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -7,7 +7,7 @@ import Item from '../Components/Item/Item'
 export const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
     return (
-        <div className="shop-category">
+        <div className="shopcategory">
             <img className='shopcategory-banner' src={props.banner} alt=""/>
             <div className="shopcategory-indexSort">
                 <p>
@@ -16,15 +16,18 @@ export const ShopCategory = (props) => {
                 <div className="shopcategory-sort">
                     Sort by <img src={dropdown_icon} alt=""/>
                 </div>
-                <div className="shopcategory-products">
-                    {all_product.map((item, i) => {
-                        if (props.category === item.category) {
-                            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-                        } else {
-                            return null;
-                        }
-                    })}
-                </div>
+            </div>
+            <div className="shopcategory-products">
+                {all_product.map((item, i) => {
+                    if (props.category === item.category) {
+                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+            <div className="shopcategory-loadmore">
+                Explore More
             </div>
         </div>
     )
