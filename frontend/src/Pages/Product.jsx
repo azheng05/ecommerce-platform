0f2ae8d5291ac24356a1d85118f4d38d488b339b
Anna Zheng <azheng38@wisc.edu>
2025-05-28 18:43:07 -0500

    Added breadcrumb

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
 
 export const ProductPage = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
-    <div>ProductPage</div>
+    <div className="product">
+      <Breadcrum product={product}/>
+    </div>
   )
 }
 
