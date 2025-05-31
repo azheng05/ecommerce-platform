2673011ade541d712314451a3b527920fa3cfdc1
Anna Zheng <azheng38@wisc.edu>
2025-05-31 03:58:22 -0500

    Added product listing overall design

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -11,24 +11,24 @@ const ProductDisplay = (props) => {
                 <img src={product.image} alt=""/>
                 <img src={product.image} alt=""/>
                 <img src={product.image} alt=""/>
-                <img src={product.image} alt=""/>
-                <img src={product.image} alt=""/>
             </div>
             <div className="productdisplay-img">
                 <img className="productdisplay-main-img" src={product.image} alt=""/>
             </div>
         </div>
-        <div className="product-display-right">
+        <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
-                <MdStar/>
-                <MdStar/>
-                <MdStar/>
-                <MdStar/>
-                <MdStarBorder/>
+            <div className="productdisplay-right-stars">
+                <div className="productdisplay-right-stars-list">
+                    <MdStar/>
+                    <MdStar/>
+                    <MdStar/>
+                    <MdStar/>
+                    <MdStarBorder/>
+                </div>
                 <p>(122)</p>
             </div>
-            <div className="productdisaply-right-prices">
+            <div className="productdisplay-right-prices">
                 <div className="productdisplay-right-price-old">
                     ${product.old_price}
                 </div>
@@ -40,8 +40,8 @@ const ProductDisplay = (props) => {
                 description of item
             </div>
             <button>Add to cart</button>
-            <p className="productdisplay-right-category"><span>Category:</span> Women, T-Shirt, Crop-top</p>
-            <p className="productdisplay-right-category"><span>Tags:</span> Plushie, Latest</p>
+            <p className="productdisplay-right-category"><span>Category:</span> Plushie, Mini</p>
+            <p className="productdisplay-right-category"><span>Tags:</span> Plushie, Latest, Blue</p>
         </div>
     </div>
   )
