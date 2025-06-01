84c57ac5ad031b10a5c50a690f75b1eb85a7b1eb
Anna Zheng <azheng38@wisc.edu>
2025-06-01 14:10:54 -0500

    Added cart functions (adding and removing)

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -4,47 +4,49 @@ import { MdStarBorder, MdStar } from "react-icons/md";
 
 const ProductDisplay = (props) => {
     const {product} = props;
-  return (
-    <div className="productdisplay">
-        <div className="productdisplay-left">
-            <div className="productdisplay-img-list">
-                <img src={product.image} alt=""/>
-                <img src={product.image} alt=""/>
-                <img src={product.image} alt=""/>
-            </div>
-            <div className="productdisplay-img">
-                <img className="productdisplay-main-img" src={product.image} alt=""/>
-            </div>
-        </div>
-        <div className="productdisplay-right">
-            <h1>{product.name}</h1>
-            <div className="productdisplay-right-stars">
-                <div className="productdisplay-right-stars-list">
-                    <MdStar/>
-                    <MdStar/>
-                    <MdStar/>
-                    <MdStar/>
-                    <MdStarBorder/>
+    const {addToCart} = useContext(ShopContext);
+
+    return (
+        <div className="productdisplay">
+            <div className="productdisplay-left">
+                <div className="productdisplay-img-list">
+                    <img src={product.image} alt=""/>
+                    <img src={product.image} alt=""/>
+                    <img src={product.image} alt=""/>
                 </div>
-                <p>(122)</p>
-            </div>
-            <div className="productdisplay-right-prices">
-                {/* <div className="productdisplay-right-price-old">
-                    ${product.old_price}
-                </div> */}
-                <div className="productdisplay-right-price-new">
-                    ${product.new_price}
+                <div className="productdisplay-img">
+                    <img className="productdisplay-main-img" src={product.image} alt=""/>
                 </div>
             </div>
-            <div className="productdisplay-right-description">
-                description of item
+            <div className="productdisplay-right">
+                <h1>{product.name}</h1>
+                <div className="productdisplay-right-stars">
+                    <div className="productdisplay-right-stars-list">
+                        <MdStar/>
+                        <MdStar/>
+                        <MdStar/>
+                        <MdStar/>
+                        <MdStarBorder/>
+                    </div>
+                    <p>(122)</p>
+                </div>
+                <div className="productdisplay-right-prices">
+                    {/* <div className="productdisplay-right-price-old">
+                        ${product.old_price}
+                    </div> */}
+                    <div className="productdisplay-right-price-new">
+                        ${product.new_price}
+                    </div>
+                </div>
+                <div className="productdisplay-right-description">
+                    description of item
+                </div>
+                <button onClick={() => {addToCart(product.id)}}>Add to cart</button>
+                <p className="productdisplay-right-category"><span>Category:</span> Plushie, Mini</p>
+                <p className="productdisplay-right-category"><span>Tags:</span> Plushie, Latest, Blue</p>
             </div>
-            <button>Add to cart</button>
-            <p className="productdisplay-right-category"><span>Category:</span> Plushie, Mini</p>
-            <p className="productdisplay-right-category"><span>Tags:</span> Plushie, Latest, Blue</p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDisplay
